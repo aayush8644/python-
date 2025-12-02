485a129a5329cdd786f88dd58056d6259b0d2293
Aayush chaurasiya <aayush9812286449@gamil.com>
2025-12-02 10:07:39 +0545

    this is fibonacci code

--- content2.py
+++ content2.py
@@ -1,1 +1,12 @@
-print("Hello, World!")
+def fib_recursive(n):
+    if n <= 0:
+        return 0
+    elif n == 1:
+        return 0
+    elif n == 2:
+        return 1
+    else:
+        return fib_recursive(n-1) + fib_recursive(n-2)
+
+# Get nth Fibonacci number
+print(fib_recursive(10))  # Output: 34
